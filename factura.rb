060859e893008b4bdb5217062e5745966242d990
Yaokiski <icastillo.cali@gmail.com>
2020-11-17 22:58:43 +0000

    Feature 2: Validar que parámetro uno (Cantidad) sea numérico.

--- factura.rb
+++ factura.rb
@@ -1,11 +1,29 @@
 class Factura
     def initialize(entries)
-        
         if(entries.length < 3)
             puts "Favor de proporcionar todos los parámetros para la facturación. \nUsage: Cantidad, Precio Unitario y Estado."
             exit
         end
+        @cantidad = entries[0]
     end
+
+    //
+    def inputValidation()
+        validateCantidad()
+    end
+    
+    def validateCantidad()
+        if (!isNumeric?(@cantidad))
+            puts "Favor de proporcionar el parámetro Cantidad en formato numérico."
+            exit
+        end
+    end
+
+    def isNumeric?(value)
+        Float(value) != nil rescue false
+    end
+
 end
 
 facturacion = Factura.new(ARGV)
+facturacion.inputValidation()
